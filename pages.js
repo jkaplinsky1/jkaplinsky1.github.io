5f80bc79fd6d6d8e4a83cbda09e470b14afcbd88
Josh Kaplinsky <kaplinsky@wisc.edu>
2020-12-30 22:05:46 -0600

    init

---
+++ pages.js
@@ -0,0 +1,15 @@
+const ghpages = require("gh-pages");
+const pathname = `${__dirname}/build`;
+const repoURL = "https://github.com/jkaplinsky1/jkaplinsky1.github.io.git";
+
+ghpages.publish(
+  pathname,
+  {
+    branch: "master",
+    repo: repoURL,
+  },
+  (err) => {
+    if (err) console.log("ERROR: ", err);
+    else console.log("PUBLISHED");
+  }
+);
---
+++ src/editable-stuff/config.js
@@ -0,0 +1,127 @@
+// Navigation Bar SECTION
+const navBar = {
+  show: true,
+};
+
+// Main Body SECTION
+const mainBody = {
+  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+  firstName: "Joshua",
+  middleName: "Brian",
+  lastName: "Kaplinsky",
+  message: " Changing the world, one <line \\> at a time. ",
+  icons: [
+    {
+      image: "fa-github",
+      url: "https://github.com/jkaplinsky1",
+    },
+    {
+      image: "fa-linkedin",
+      url: "https://www.linkedin.com/in/joshkaplinsky/",
+    },
+  ],
+};
+
+// ABOUT SECTION
+// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
+//a) your Instagram username
+//      i.e:profilePictureLink:"johnDoe123",
+//b) a link to an hosted image
+//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
+//c) image in "editable-stuff" directory and use require("") to import here,
+//      i.e: profilePictureLink: require("../editable-stuff/hashirshoaeb.png"),
+//d) If you do not want any picture to be displayed, just leave it empty :)
+//      i.e: profilePictureLink: "",
+// For Resume either provide link to your resume or import from "editable-stuff" directory
+//     i.e resume: require("../editable-stuff/resume.pdf"),
+//         resume: "https://docs.google.com/document/d/16apx8s6zlVwCu7Aea6jdJtXOnAb6GsNP8QAsTv7gWZo/edit",
+
+const about = {
+  show: true,
+  heading: "About Me",
+  imageLink: require("../editable-stuff/headshot.jpeg"),
+  imageSize: 375,
+  message:
+    "I am a Software Engineer with a passions for modern technology. My drive to learn new skills, experience in problem solving, creativity and leadership sets me apart and brings a new prospective to any team. ",
+  resume: require("../editable-stuff/resume.pdf"),
+};
+
+// PROJECTS SECTION
+// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
+//      i.e: reposLength: 0,
+// If you want to display specfic projects, add the repository names,
+//      i.e ["repository-1", "repo-2"]
+// const repos = {
+//   show: false,
+//   heading: "Projects",
+//   gitHubUsername: "jkaplinsky1", //i.e."johnDoe12Gh"
+//   reposLength: 4,
+//   specificRepos: [],
+// };
+
+// Leadership SECTION
+// const leadership = {
+//   show: false,
+//   heading: "Leadership",
+//   message:
+//     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+//   images: [
+//     { 
+//       img: require("../editable-stuff/headshot.jpeg"), 
+//       label: "First slide label", 
+//       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+//     },
+//     { 
+//       img: require("../editable-stuff/headshot.jpeg"), 
+//       label: "Second slide label", 
+//       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+//     },
+//   ],
+//   imageSize: {
+//     width:"615",
+//     height:"450"
+//   }
+// };
+
+// SKILLS SECTION
+// const skills = {
+//   show: false,
+//   heading: "Skills",
+//   hardSkills: [
+//     { name: "Golang", value: 100 },
+//     { name: "Java", value: 80 },
+//     { name: "Python", value: 80 },
+//     { name: "C/C++", value: 65 },
+//     { name: "JavaScript", value: 90 },
+//     { name: "React", value: 65 },
+//     { name: "HTML/CSS", value: 55 },
+
+//   ],
+//   softSkills: [
+//     { name: "Goal-Oriented", value: 80 },
+//     { name: "Collaboration", value: 90 },
+//     { name: "Positivity", value: 75 },
+//     { name: "Adaptability", value: 85 },
+//     { name: "Problem Solving", value: 75 },
+//     { name: "Empathy", value: 90 },
+//     { name: "Organization", value: 70 },
+//     { name: "Creativity", value: 90 },
+//   ],
+// };
+
+// GET IN TOUCH SECTION
+// const getInTouch = {
+//   show: false,
+//   heading: "Projects",
+//   message:
+//     "Entity Builder",
+//   // email: "josh@joshkaplinsky.com",
+// };
+
+// Blog SECTION
+// const blog = {
+//   show: false,
+// };
+
+// export { navBar, mainBody, about, repos, skills, leadership, getInTouch };
+export { navBar, mainBody, about };
