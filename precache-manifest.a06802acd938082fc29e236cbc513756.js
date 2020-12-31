f6497c8fb1e313bad9c113f203e7acc8aec469e3
Josh Kaplinsky <kaplinsky@wisc.edu>
2020-12-30 22:07:17 -0600

    Updates

---
+++ precache-manifest.a06802acd938082fc29e236cbc513756.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23deb1b0c9f36e14a7663a4999ce08a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20c0f7d6678a8749a2c1",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "8d90d772c38a4b145b6f",
+    "url": "/static/js/2.c6a3df00.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c6a3df00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20c0f7d6678a8749a2c1",
+    "url": "/static/js/main.8a102bc1.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "f0a057dc010f9981ed31eff39d112732",
+    "url": "/static/media/headshot.f0a057dc.jpeg"
+  },
+  {
+    "revision": "30313dab7c58399231b792026139b345",
+    "url": "/static/media/resume.30313dab.pdf"
+  }
+]);
